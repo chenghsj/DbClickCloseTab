a512670207e39525ef939d54228d9887254e18ba
Cheng <chengjohnsonhs@gmail.com>
2022-04-18 00:22:56 +0800

    change variable name

--- inject.js
+++ inject.js
@@ -7,35 +7,36 @@ function getStoragePromise() {
   });
 }
 
-const init = async function () {
-  var timeInterval;
-  try {
-    timeInterval = await getStoragePromise();
-  } catch (err) {
-    // console.error(err);
-    timeInterval = 400;
-  }
-  let handleDblclick = function () {
-    chrome.runtime.sendMessage({ closeTab: true });
-  };
-  window.addEventListener("auxclick", makeDoubleClick(handleDblclick), false);
-
-  function makeDoubleClick(cb) {
-    //https://stackoverflow.com/questions/46812360/how-to-detect-double-click-on-element-by-registering-only-click-event
-    var clicks = 0,
-      timeout;
-    return function () {
-      clicks++;
-      if (clicks == 1) {
-        timeout = setTimeout(function () {
-          clicks = 0;
-        }, timeInterval);
-      } else {
-        timeout && clearTimeout(timeout);
-        cb && cb();
-        clicks = 0;
-      }
+var dblMiddleClickInit =
+  dblMiddleClickInit ||
+  async function () {
+    var timeInterval;
+    try {
+      timeInterval = await getStoragePromise();
+    } catch (err) {
+      // console.error(err);
+      timeInterval = 400;
+    }
+    let handleDblclick = function () {
+      chrome.runtime.sendMessage({ closeTab: true });
     };
-  }
-};
-init();
+    window.addEventListener("auxclick", makeDoubleClick(handleDblclick), false);
+
+    function makeDoubleClick(cb) {
+      var clicks = 0,
+        timeout;
+      return function () {
+        clicks++;
+        if (clicks == 1) {
+          timeout = setTimeout(function () {
+            clicks = 0;
+          }, timeInterval);
+        } else {
+          timeout && clearTimeout(timeout);
+          cb && cb();
+          clicks = 0;
+        }
+      };
+    }
+  };
+dblMiddleClickInit();
